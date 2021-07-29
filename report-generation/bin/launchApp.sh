9ff9f77a2749b86c87f84ff1719ac82dc7504db8
Porwal, Utkarsh <utkarsh.porwal@emc.com>
2021-07-30 01:16:00 +0530

    Created an application launcher shell script

---
+++ report-generation/bin/launchApp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Utkarsh Porwal
+#Launches the application using their jar files.
+
+echo "Launching key-service at"  `date`
+nohup java -jar ../key-service/target/key-service-0.0.1-SNAPSHOT.jar &
+sleep 5
+echo "Finished launching key-service."
+
+echo "Launching transaction-service at"  `date`
+nohup java -jar ../transaction-service/target/transaction-service-0.0.1-SNAPSHOT.jar &
+sleep 15
+echo "Finished launching transaction-service."
+
+echo "Report Generation Scheduler at " `date`
+java -jar ../report-executor/target/report-executor-0.0.1-SNAPSHOT.jar
+echo
+echo
+echo "Report Generation finished. Please check them at `pwd`"
